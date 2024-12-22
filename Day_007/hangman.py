eb948dbe7e9afe92653f56be80ba946b189ce1be
Bharathi Pravallika <52744796+BharathiPravallika@users.noreply.github.com>
2024-12-22 12:23:59 +0530

    Update hangman.py

--- Day_007/hangman.py
+++ Day_007/hangman.py
@@ -20,18 +20,22 @@ game_over = False
 correct_letters = []
 
 while not game_over:
+    print(f"********** {no_of_lives_left} LIVES LEFT **********")
     guess = input("Guess the letter: ").lower()
-    print(guess)
+    if guess in correct_letters:
+        print(f"You 've already guessed {guess}")
+    # print(guess)
     
     display = ""
     
     for letter in chosen_word:
         if letter == guess:
             display += letter
-            if letter not in correct_letters:
-                correct_letters.append(letter)
-            else:
-                print(f"You have already guessed {letter} try another one!")
+            correct_letters.append(letter)
+            # if letter not in correct_letters:
+            #     correct_letters.append(letter)
+            # else:
+            #     print(f"You have already guessed {letter} try another one!")
         elif letter in correct_letters:
             display += letter
         else:
@@ -40,7 +44,7 @@ while not game_over:
     if guess not in chosen_word:
             print(f"you guessed {guess}, that is not in the word. You lose a life!")
             no_of_lives_left -= 1
-            print(f"********** {no_of_lives_left} LIVES LEFT **********")
+            # print(f"********** {no_of_lives_left} LIVES LEFT **********")
             if no_of_lives_left == 0:
                 game_over = True
                 print("********** YOU LOSE **********")
